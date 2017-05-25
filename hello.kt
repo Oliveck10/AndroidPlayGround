93646a4b1a5e61f6ffa555a78f0df84a651d6a36
Oliveck10 <oliveck10@gmail.com>
2017-05-25 17:19:07 +0800

    step on for first try on kotlin as future android development

---
+++ hello.kt
@@ -0,0 +1,26 @@
+/* 
+    The first app for kotlin tutorials
+*/
+
+fun main(args:Array<String>) {
+// Enter user name
+    print("Enter name: ")
+    var name = readLine()
+// Enter user age
+    print("Enter age: ")
+    var age:Int = readLine()!!.toInt()
+// Enter user department
+    print("Enter department: ")
+    var dep:String?
+    dep = readLine()
+// Enter number of pi
+    print("Enter pi: ")
+    var pi:Double = readLine()!!.toDouble()
+
+    println("\n*******output*******\n")
+    println("name: " + name)
+    println("age: " + age)
+    println("department: " + dep)
+    println("pi: " + pi)
+
+}

f075f6a4506fc1c18f7911d36f8f13ffb1530cc5
Oliveck10 <oliveck10@gmail.com>
2017-05-26 16:07:32 +0800

    revise className of hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@
   - "javac" to compile java source code and "java" to execute compiled file
   -  class name should be same as file name => every class is an object
 */
-public class test {
+public class hello {
 	public static void main(String[] args){
 		System.out.println("I'm a Simple Program");
   	}

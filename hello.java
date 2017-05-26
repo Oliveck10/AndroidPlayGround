367cdb62ec8ce2a54594ad2c9cbf427fc0315a0f
Oliveck10 <oliveck10@gmail.com>
2017-05-26 15:39:50 +0800

    create first java file to practice java again

---
+++ hello.java
@@ -0,0 +1,9 @@
+/* 
+  - "javac" to compile java source code and "java" to execute compiled file
+  -  class name should be same as file name => every class is an object
+*/
+public class test {
+	public static void main(String[] args){
+		System.out.println("I'm a Simple Program");
+  	}
+}

f5d30d0f9623af6f100128eaad1463b9fbba5792
Oliveck10 <oliveck10@gmail.com>
2017-09-03 00:03:06 +0800

    link java file to layout xml file

--- ToDoList/app/src/main/java/com/example/pengyihao/todolist/MainActivity.java
+++ ToDoList/app/src/main/java/com/example/pengyihao/todolist/MainActivity.java
@@ -1,8 +1,22 @@
 package com.example.pengyihao.todolist;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 /**
  * Created by pengyihao on 2017/9/2.
  */
 
-public class MainActivity {
+// Activity is one of the classes under android SDK
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main); // contentview populate to the file activity_main.xml
+
+    }
+
 }

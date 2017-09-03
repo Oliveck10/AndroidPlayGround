78a4b2dfd6716f2bbcb1d595dd21caeec51ea6c4
Oliveck10 <oliveck10@gmail.com>
2017-09-03 14:45:27 +0800

    add getView method and tell how many items the listview has to display (dummy data)

--- ToDoList/app/src/main/java/com/example/pengyihao/todolist/StringAdapter.java
+++ ToDoList/app/src/main/java/com/example/pengyihao/todolist/StringAdapter.java
@@ -1,9 +1,15 @@
 package com.example.pengyihao.todolist;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.content.Context;
 import java.util.List;
+import android.widget.TextView;
+
 
 import java.util.ArrayList;
 
@@ -16,7 +22,7 @@ public class StringAdapter extends ArrayAdapter<String> {
     private int mTextViewResource;
 
     private LayoutInflater mLayoutInflater;
-    private List<String> mitems = new ArrayList<String>();
+    private List<String> mItems = new ArrayList<String>();
 
     public StringAdapter(Context context, int resource, int textViewResource){
         super(context, resource, textViewResource);
@@ -25,6 +31,26 @@ public class StringAdapter extends ArrayAdapter<String> {
         mTextViewResource = textViewResource;
 
         mLayoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        // add dummy data
+        mItems.add("AAA");
+        mItems.add("BBB");
     }
 
+    @Override
+    public int getCount() {
+        return mItems.size();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = mLayoutInflater.inflate(mResource, null);
+        }
+        TextView textView = (TextView) convertView.findViewById(mTextViewResource);
+        String item = mItems.get(position);
+        textView.setText(item);
+
+        return convertView;
+    }
 }
